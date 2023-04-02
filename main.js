628dc874e9958bc653588ef64716b50b40f7c80c
mayarazan <102388977+mayarazan@users.noreply.github.com>
2023-04-02 21:33:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,19 @@ let link = document.getElementById('link')
 li = document.querySelectorAll('li')
 let img = document.getElementById('img')
 let about = document.getElementById('about')
+let section = document.getElementById('section')
+let contact = document.getElementById('contact')
+let service = document.getElementById('service')
+let skills = document.getElementById('skills')
 
 bar.onclick = function(){
     link.style.display = 'flex'
+    about.style.display = 'none'
+    section.style.display = 'none'
+    contact.style.display = 'none'
+    service.style.display = 'none'
+    skills.style.display = 'none'
+
 }
 
 // if(window.innerWidth < 992){
@@ -18,7 +28,12 @@ for(let i = 0; i < li.length; i++){
     li[i].onclick = function(){
         if(window.innerWidth < 675){
             link.style.display = 'none';
+            about.style.display = 'block';
+            section.style.display = 'block';
+            contact.style.display = 'block';
+            service.style.display = 'block';
+            skills.style.display = 'block';
+            
         }
     }
 }
-
